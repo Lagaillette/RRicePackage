d211ffdee6e402318a3cdd346c9bc85d792c5556
vautrinss <baptistevautrin52@gmail.com>
2017-06-07 13:52:15 +0700

    Script Local OK

--- Python/ScriptTable/RapTOLoc.py
+++ Python/ScriptTable/RapTOLoc.py
@@ -9,12 +9,39 @@ array = pd.read_csv(file, sep="\t", header = None)
 #Named columns
 array.columns = ["RAP", "LOC"]
 
-#Save the entered RAP ID
-RapID = input('RAP ID : ')
-print("\n")
 
-#Find the line corresponding to the entered RAP ID
-data = array.loc[array['RAP'] == RapID]
+while 1:
+    choice = input('\n\n1Choose your conversion :\n 1)RAP to LOC\n 2)LOC to RAP\n 3)Exit\n')
 
-#Print the corresponding LOC ID
-print(data["LOC"])
+    if(choice == '1'):
+
+        #Save the entered RAP ID
+        RapID = input('RAP ID : ')
+        print("\n")
+
+        #Find the line corresponding to the entered RAP ID
+        data = array.loc[array['RAP'] == RapID]
+
+        #Print the corresponding LOC ID
+        print(data["LOC"])
+
+    elif (choice == '2'):
+
+        #Save the entered RAP ID
+        RapID = input('LOC ID : ')
+        print("\n")
+
+        #Find the line corresponding to the entered RAP ID
+        data = array.loc[array['LOC'] == RapID]
+
+        #Print the corresponding LOC ID
+        print(data["RAP"])
+
+    elif (choice == '3'):
+
+        #End of the loop
+        raise SystemExit(0)
+
+
+    else:
+        print("Error ! Choose 1, 2 or 3")
