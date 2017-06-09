b38e0d7efe9563aad195823047963776e8cccaff
vautrinss <baptistevautrin52@gmail.com>
2017-06-09 11:03:39 +0700

    Script V3 OK

--- Python/ScriptTable/ScriptV3.py
+++ Python/ScriptTable/ScriptV3.py
@@ -7,60 +7,112 @@ import gzip
 import requests
 import io
 
-#If not URL specify, there is a default URL
-def loadFileURL(url="http://rapdb.dna.affrc.go.jp/download/archive/RAP-MSU_2017-04-14.txt.gz"):
 
-    # local file in the same folder
-    #url = "http://rapdb.dna.affrc.go.jp/download/archive/RAP-MSU_2017-04-14.txt.gz"
+class RRice:
 
-    # Give the entire name of the file with the extension .gz
-    filename = url.split("/")[-1]
+    #constructor
+    def __init__(self):
+        self.url = ""
+        self.nameFile = ""
 
-    # Give the name of the file without .gz
-    uncompressName = filename[:-3]
+    def loadFileURL(self, url):
 
-    # Fetch the file by the url and decompress it
-    r = requests.get(url)
-    decompressedFile = gzip.decompress(r.content)
+        """
+        Download the file located in the URL
 
-    # Create the file .txt
-    with open(uncompressName, "wb") as f:
-        f.write(decompressedFile)
-        f.close()
+        :param url: The url for accessing the file
+        :type url: String
+        """
 
-    # Return the name of the created file
-    return uncompressName
+        self.url = url
+        filename = self.url.split("/")[-1]
+
+        # Give the name of the file without .gz
+        uncompressName = filename[:-3]
+
+        # Fetch the file by the url and decompress it
+        r = requests.get(self.url)
+        decompressedFile = gzip.decompress(r.content)
+
+        # Create the file .txt
+        with open(uncompressName, "wb") as f:
+            f.write(decompressedFile)
+            f.close()
+
+        # Return the name of the created file
+        self.nameFile = uncompressName
+        #return uncompressName
 
 
-def rapToLoc(RAPID):
+    def rapToLoc(self, RAPID):
 
-    uncompressName = loadFileURL("http://rapdb.dna.affrc.go.jp/download/archive/RAP-MSU_2017-04-14.txt.gz")
-    # Use the previous created file (.txt)
-    with open(uncompressName, "r+b") as file:
+        """
+        Give the LOC ID that correspond to the entered RAP ID
 
-        # Import file tab-delimited
-        try:
-            array = pd.read_csv(file, sep="\t", header=None)
-        except EmptyDataError:
-            array = pd.DataFrame()
-        # Named columns
-        array.columns = ["RAP", "LOC"]
+        :param RAPID: the RAP ID that you want the corresponding LOC ID
+        :type RAPID: String
+        """
 
-        # Save the entered RAP ID
-        RapID = input('RAP ID : ')
-        print("\n")
+        # Use the previous created file (.txt)
+        with open(self.nameFile, "r+b") as file:
 
-        # Find the line corresponding to the entered RAP ID (Select LOC FROM LOC where RAP = RapID)
-        data = array.loc[array['RAP'] == RapID]
+            # Import file tab-delimited
+            try:
+                array = pd.read_csv(file, sep="\t", header=None)
+            except EmptyDataError:
+                array = pd.DataFrame()
+            # Named columns
+            array.columns = ["RAP", "LOC"]
 
-        # Store the corresponding LOC ID and split the string
-        result = data['LOC'].str.split(',').str
+            # Find the line corresponding to the entered RAP ID (Select LOC FROM LOC where RAP = RapID)
+            data = array.loc[array['RAP'] == RAPID]
 
-        # reinitialize i
-        i = 0
-        # Loop for printing the result
-        while (i < int(result.len())):
-            print("LOC ID : " + result[i].values, end=' ', flush=True)
-            i = i + 1
+            # Store the corresponding LOC ID and split the string
+            result = data['LOC'].str.split(',').str
 
-        file.close()
+            # reinitialize i
+            i = 0
+            # Loop for printing the result
+            while (i < int(result.len())):
+                print("LOC ID : " + result[i].values, end=' ', flush=True)
+                i = i + 1
+            print("\n")
+
+            file.close()
+
+
+    def locToRap(self, LOCID):
+        """
+        Give the RAP ID that correspond to the entered LOC ID
+
+        :param LOCID: The LOC ID that you want te corresponding RAP ID
+        :type LOCID: String
+        """
+
+        # Use the previous created file (.txt)
+        with open(self.nameFile, "r+b") as file:
+
+            # Import file tab-delimited
+            try:
+                array = pd.read_csv(file, sep="\t", header=None)
+            except EmptyDataError:
+                array = pd.DataFrame()
+            # Named columns
+            array.columns = ["RAP", "LOC"]
+
+            # Find the line corresponding to the entered LOC ID (Select rap From RAP where LOC like LOCID)
+            data = array[array['LOC'].str.contains(LOCID)]
+
+            # Print the corresponding LOC ID
+            print("RAP ID : " + data["RAP"].values, end=' ', flush=True)
+            print("\n")
+
+            file.close()
+
+    def informations(self):
+        """
+            Print the informations of the class
+        """
+        print("Name : "+self.nameFile)
+        print("URL : " +self.url)
+
