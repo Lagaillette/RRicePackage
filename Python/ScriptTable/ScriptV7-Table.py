8248fc0d97cf0c3c123f61b20cbc3b4ee1c4e22c
vautrinss <baptistevautrin52@gmail.com>
2017-06-14 17:15:12 +0700

    Dictionnary

--- Python/ScriptTable/ScriptV7-Table.py
+++ Python/ScriptTable/ScriptV7-Table.py
@@ -23,36 +23,30 @@ class RRice():
         result = soup.find('tr', attrs={"class": "result"})
 
         self.id = result.find('td', attrs={"class": "c01"}).a.contents[0]
-        print("ID : " + self.id)
         self.description = result.find('td', attrs={"class": "c02"}).contents[0]
-        print("Description : "+self.description)
         self.position = result.find('td', attrs={"class": "c03"}).a.contents[0]
-        print("Position : "+self.position)
         self.RAP_symbol = result.find('td', attrs={"class": "c04"}).contents
-        print(self.RAP_symbol)
         self.RAP_name = result.find('td', attrs={"class": "c05"}).contents
-        print(self.RAP_name)
         self.CGSNL_symbol = result.find('td', attrs={"class": "c06"}).contents[0]
-        print("CGSNL Gene Symbol : "+self.CGSNL_symbol)
         self.CGSNL_name = result.find('td', attrs={"class": "c07"}).contents[0]
-        print("CGSNL Gene Name : "+self.CGSNL_name)
         self.Oryzabase_symbol = result.find('td', attrs={"class": "c08"}).contents[0]
-        print("Oryzabase Gene Symbol Synonym(s) : "+self.Oryzabase_symbol)
         self.Oryzabase_name = result.find('td', attrs={"class": "c09"}).contents[0]
-        print("Oryzabase Gene Name Synonym(s) : "+self.Oryzabase_name)
 
-    # It doesn't work because of null tab
-    def informations(self):
-        print(
-              "ID : "+self.id+
-              "\nDescription : "+self.description+
-              "\nPosition : "+self.position+
-              "\nRAP-DB Gene Symbol Synonym(s) : " + self.RAP_symbol+
-              "\nRAP-DB Gene Name Synonym(s) : " + self.RAP_name+
-              "\nCGSNL Gene Symbol : "+self.CGSNL_symbol+
-              "\nCGSNL Gene Name : "+self.CGSNL_name+
-              "\nOryzabase Gene Symbol Synonym(s) : "+self.Oryzabase_symbol+
-              "\nOryzabase Gene Name Synonym(s) : "+self.Oryzabase_name)
+
+        hashmap = {"ID" : self.id,
+                   "Description" : self.description,
+                   "Position" : self.position,
+                   "RAP-DB Gene Symbol Synonym(s)" : self.RAP_symbol,
+                   "RAP-DB Gene Name Synonym(s)" : self.RAP_name,
+                   "CGSNL Gene Symbol" : self.CGSNL_symbol,
+                   "CGSNL Gene Name" : self.CGSNL_name,
+                   "Oryzabase Gene Symbol Synonym(s)" : self.Oryzabase_symbol,
+                   "Oryzabase Gene Name Synonym(s)" : self.Oryzabase_name
+                   }
+
+        print(hashmap)
+        print(len(hashmap))
+        return hashmap
 
 
 
