77b91dc666550c84f3a43a61f5c7c2f1539c3935
vautrinss <baptistevautrin52@gmail.com>
2017-06-15 10:44:43 +0700

    Packaging

---
+++ Python/RRiceBeta/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RRiceBeta',
+      version='1.0',
+      description='RRice package',
+      url='https://github.com/Lagaillette/RRicePackage/tree/master/Python/ScriptTable',
+      author='Baptiste VAUTRIN',
+      author_email='baptiste.vautrin@gmail.com',
+      license='ICTLAB - IRD',
+      packages=['RRiceBeta'],
+      zip_safe=False)
