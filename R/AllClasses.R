b8335a8d8a918fdc273939d59ca833025619219f
julien Gallego <maths.ju@outlook.com>
2017-06-23 07:03:47 +0200

    AllClasses file

---
+++ R/AllClasses.R
@@ -0,0 +1,54 @@
+#' The Experiment class.
+#'
+#' Some details about this class and my plans for it in the body.
+#'
+#' @slot name The name of the experiment
+#' @slot date The date of the experiment
+#' @slot databases the databases which concern the object we are studying
+#' @slot genes The list of genes we avec download.
+#' @slot others The others attributes the user can add and delete
+#' @name Experiment
+#' @rdname Experiment-class
+#' @docType classes
+#' @exportClass Experiment
+setClass(
+  #name of the class
+  "Experiment",
+  
+  #attributes of the class
+  representation = representation(name = "character", date = "Date", databases = "list", genes = "list", others = "list")
+)
+
+#' The Gene class.
+#'
+#' A gene contains a lot of details that depend on the database it belongs. 
+#' But every gens have information in common. So we put all these informations
+#' into a global class "gene" from which the others specific gene class 
+#' inherit.
+#'
+#' @slot id The id of the gene. Many genes can have the same id because there
+#' are the same in spite of the different attributes they have.
+#' @slot locus The locus the gene belongs.
+#' @slot others The others attributes the user want to add or delete.
+#' @slot genes The list of genes we avec download.
+#' @name Gene
+#' @rdname Gene-class
+#' @exportClass Gene
+setClass(
+  #name of the class
+  "Gene",
+  
+  #attributes of the class
+  slots = list(uniquename = "character", locus = "data.frame", others = "list")
+)
+
+
+setClass(
+  #name of the class
+  "GeneDB1",
+  
+  #attributes of the classnumeric
+  slots = list(msU7name = "character", fgeneshName = "character", rappredname = "character", fmin = "numeric", fmax = "numeric",contig = "character",iricname = "character", strand = "numeric", description = "character"),
+  
+  contains = "Gene"
+)
