0b85112e30a142fde2ac1a7325790a46258b03cc
antoinepari <parisantoine78@gmail.com>
2017-07-04 11:55:05 +0700

    Add whichone function + existsGene

--- R/functions-basics.R
+++ R/functions-basics.R
@@ -1,10 +1,27 @@
+#This function is used in methods-calldb to know if a gene belongs to the liste (genes)
 existsGene <- function(genes, id){
     found <- FALSE
     i <- 1
-    while(!found && i<length(genes)){
-        if (genes[i]@id == id){
+    while(!found && i<=length(genes)){
+        if (as.character(genes[[i]]@id) == as.character(id)){
             found <- TRUE
         }
+        i <- i+1
     }
     return(found)
+}
+
+
+#this function allow us to know if the user is using Linux or Windows
+whichone <- function(){
+  found <- 0
+  
+  debut = getwd()
+  if (substr(debut, 0,1) == '/') #ubuntu
+    found <- 1
+  else #windows
+    found <- 0
+  
+  print(found)
+  return(found)
 }
