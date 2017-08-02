6ac0a93bab5837c3c804c7b8f57d76aad0833cf3
antoinepari <parisantoine78@gmail.com>
2017-08-02 10:40:56 +0200

    other examples

--- R/callSnpSeek-function.R
+++ R/callSnpSeek-function.R
@@ -163,9 +163,9 @@ getIds <- function (i, locusList) {
 #' @export
 #' @rdname callSnpSeek-function
 #' @examples 
-#' locusList <- data.frame(ch = c("1","1"),
-#'                         st = c("148907","527906"),
-#'                         end = c("248907","842359"))
+#' locusList <- data.frame(ch = c("1"),
+#'                         st = c("148907"),
+#'                         end = c("248907"))
 #'                   
 #' callSnpSeek(locusList)
 callSnpSeek <- function(locus){
